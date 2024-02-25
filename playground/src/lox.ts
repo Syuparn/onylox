425c76949412ac1aa39399d810b3bba0b56b294c
syuparn <s.hello.spagetti@gmail.com>
2024-02-25 12:11:34 +0900

    fix import

--- playground/src/lox.ts
+++ playground/src/lox.ts
@@ -1,13 +1,13 @@
-// HACK: import dynamically to avoid `ReferenceError: document is not defined`
-// https://docs.wasmer.io/javascript-sdk/explainers/troubleshooting#referenceerror-document-is-not-defined
-const { init, Wasmer } = await import("@wasmer/sdk");
-
 export type LoxResult = {
   code: number;
   stdout: string;
 }
 
 export async function runLox(source: string): Promise<LoxResult>{
+  // HACK: import dynamically to avoid `ReferenceError: document is not defined`
+  // https://docs.wasmer.io/javascript-sdk/explainers/troubleshooting#referenceerror-document-is-not-defined
+  const { init, Wasmer } = await import("@wasmer/sdk");
+
   await init()
   const pkg = await Wasmer.fromRegistry('syuparn/onylox')
   const instance = await pkg.entrypoint?.run({
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// used for local development (for wasm)
+const crossOriginPolicy = {
+  name: "configure-server",
+
+  configureServer(server) {
+    server.middlewares.use((_req, res, next) => {
+      res.setHeader("Cross-Origin-Opener-Policy", "same-origin")
+      res.setHeader("Cross-Origin-Embedder-Policy", "require-corp")
+      next()
+    })
+  },
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   // HACK: import dynamically to avoid `ReferenceError: document is not defined`
@@ -9,7 +22,7 @@ export default defineConfig({
         polyfill: false,
     },
   },
-  plugins: [react()],
+  plugins: [react(), crossOriginPolicy],
   // HACK: fix link path because endpoint of GitHub pages is https://syuparn.github.io/onylox/ (not root)
   base: process.env.GITHUB_PAGES
     ? 'onylox'
